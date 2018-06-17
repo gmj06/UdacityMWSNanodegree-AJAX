a2220b8469bc1775ea84529d0c577babb1403a96
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-06-16 17:30:25 -0700

    Revert "Implemented async call using jQuery"
    This reverts commit c46e756dd28f41380180902969cf9d5170658729.
    
    Lesson 2 implemented in Lesson 1, so reverting it

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -9,67 +9,60 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
-        // const imgRequest = new XMLHttpRequest();
-        // imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        // imgRequest.onload = addImage;
-        // imgRequest.onerror = function (err) {
-        //     requestError(err, 'image');
-        // };
-        // imgRequest.setRequestHeader('Authorization', 'Client-ID 3642d03c95d50462fd0f99bc3f0ba6228412a9c9b43407e0da51e4e79fff489e')
-        // imgRequest.send();
+        const imgRequest = new XMLHttpRequest();
+        imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imgRequest.onload = addImage;
+        imgRequest.onerror = function (err) {
+            requestError(err, 'image');
+        };
+        imgRequest.setRequestHeader('Authorization', 'Client-ID 3642d03c95d50462fd0f99bc3f0ba6228412a9c9b43407e0da51e4e79fff489e')
+        imgRequest.send();
 
-        $.ajax({
-            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
-            headers: {
-                Authorization: 'Client-ID 3642d03c95d50462fd0f99bc3f0ba6228412a9c9b43407e0da51e4e79fff489e'
-            }
-        }).done(addImage);
-
-        // const articleRequest = new XMLHttpRequest();
-        // articleRequest.onload = addArticles;
-        // articleRequest.onerror = function(err){
-        //     requestError(err, 'article');
-        // };
-        // articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=18dd1e313c5a4964a8e39da338b15ce7`);
-        // articleRequest.send();
-
-        $.ajax({
-            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=18dd1e313c5a4964a8e39da338b15ce7`
-        }).done(addArticles);
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function(err){
+            requestError(err, 'article');
+        };
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=18dd1e313c5a4964a8e39da338b15ce7`);
+        articleRequest.send();
     });
 
-    function addImage(images) {
-        const firstImage = images.results[0];
+    function addImage() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+        const firstImage = data.results[0];
 
-        responseContainer.insertAdjacentHTML('afterbegin', `<figure>
-                <image src="${firstImage.urls.small}" alt="${searchedForText}" />
-                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>        
-            </figure>`
-        );
+        if (data && data.results && data.results[0]) {
+            htmlContent = `<figure>
+            <image src="${firstImage.urls.regular}" alt="${searchedForText}" />
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>        
+        </figure>`;
+        } else {
+            htmlContent = "<div class='error-no-image'>No Images Available</div>";
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        
     }
 
-    function addArticles(articles) {
-        // let htmlContent = '';
-        // //const data = JSON.parse(this.responseText);
+    function addArticles() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
 
-        // if (articles.response && articles.response.docs && articles.response.docs.length > 1) {
-        //     htmlContent = '<ul>' +
-        //         articles.response.docs.map(article => `<li class="article">
-        //         <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
-        //         <p>${article.snippet}</p>
-        //     </li>`).join('') + '</ul>';
-        // } else {
-        //     htmlContent = "<div class='error-no-articles'>No Articles Available</div>";
-        // }
-
-        // responseContainer.insertAdjacentHTML('beforeend', htmlContent);
-
-        responseContainer.insertAdjacentHTML('beforeend', '<ul>' +
-                articles.response.docs.map(article => `<li class="article">
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' +
+                data.response.docs.map(article => `<li class="article">
                 <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
                 <p>${article.snippet}</p>
-            </li>`).join('') + '</ul>'
-        );
+            </li>`).join('') + '</ul>';
+        } else {
+            htmlContent = "<div class='error-no-articles'>No Articles Available</div>";
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+
+
+
     }
 
 
